35444ccbd51f1f846f8f79956007674fff6c92b4
Amruta Satish Pawar <89676876+ASP1818@users.noreply.github.com>
2022-07-12 21:38:20 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,18 @@
+class Atm(object): 
+    def __init__(self, CashWithdrawl, BalanceEnquiry):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+
+        def start(self):
+            print("started")
+
+        def stop(self):
+            print("stop")
+
+        def accelarate(self):
+            print("accelarating.....")
+            "accelarator functionality here"
+
+        def change_gear(self, gear_type):
+            print("gear changed")
+            "gear related functionality here"
